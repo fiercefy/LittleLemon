accfe2f7c1fef17191a62e0a73ef81b93efe3254
andyfu13 <99470960+andyfu13@users.noreply.github.com>
2023-11-09 22:02:44 -0800

    update

--- littlelemon/settings.py
+++ littlelemon/settings.py
@@ -42,7 +42,7 @@ INSTALLED_APPS = [
     'djoser',
     'rest_framework.authtoken',
 ]
-f
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
